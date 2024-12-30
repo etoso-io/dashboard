b6cefdd0e7d963d397782ba5dfea76d4e2194e16
danisivayev <danis.ivayev@waveaccess.global>
2024-12-30 17:47:06 +0300

    fix typo

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,10 +3,11 @@ import type { ChloropethMapComponent } from './components/ChloropethMap/Chlorope
 import type { IndustriesChartComponent } from './components/IndustriesChart/IndustriesChart';
 import type { StandardsChartComponent } from './components/StandardsChart/StandardsChart';
 import type { TopicMentionsComponent } from './components/TopicMentions/TopicMentions';
+import type { GridContainerComponent } from './components/GridContainer/GridContainer';
 
 export declare const ChartContainer: ChartContainerComponent;
 export declare const ChloropethMap: ChloropethMapComponent;
 export declare const IndustriesChart: IndustriesChartComponent;
 export declare const StandardsChart: StandardsChartComponent;
 export declare const TopicMentions: TopicMentionsComponent;
-export declare const GridContainer: ChartContainerComponent;
+export declare const GridContainer: GridContainerComponent;
